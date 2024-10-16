ee44ab013d0db2d4c4a8327791a6789397df89bf
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-10-16 13:36:39 -0700

    Prueba para la funcion getPuntaje()

--- Desktop/Proyecto_2_GDCS/src/metrica.spec.js
+++ Desktop/Proyecto_2_GDCS/src/metrica.spec.js
@@ -8,3 +8,11 @@ describe("Test para getNumeroCommit()", () => {
       expect(metrica.getNumeroCommit()).toEqual(1);
     });
   });
+
+describe("Test para getPuntaje()", () => {
+    it("Debe retornar el puntaje predeterminado de la metrica", () => {
+      let metrica = new Metrica(1);
+  
+      expect(metrica.getPuntaje()).toEqual(0);
+    });
+  });
