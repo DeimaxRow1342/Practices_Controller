8b3e7d586e2e05b3208c6b2075ad04947ecf6301
Isaac71412079 <isaac.rivero@ucb.edu.bo>
2024-10-16 08:28:47 -0400

    Pruebas para la función obtenerPractica(nombre)

--- Desktop/Proyecto_2_GDCS/src/practicas.spec.js
+++ Desktop/Proyecto_2_GDCS/src/practicas.spec.js
@@ -16,3 +16,25 @@ describe("Test para cargarDatos()", () => {
     expect(practica.enlace).toBe(enlace);
   });
 });
+
+describe("Pruebas para la función obtenerPractica(nombre)", () => {
+  
+    it("Debe devolver la instancia cuando el nombre coincide", () => {
+      const practica = new Practicas();
+      practica.cargarDatos("Práctica TDD", "Descripción de prueba", "2024-10-15", "http://enlace.com");
+      
+      const resultado = practica.obtenerPractica("Práctica TDD");
+      
+      expect(resultado).toBe(practica);  
+    });
+    
+    it("Debe devolver undefined cuando el nombre no coincide", () => {
+      const practica = new Practicas();
+      practica.cargarDatos("Práctica TDD", "Descripción de prueba", "2024-10-15", "http://enlace.com");
+      
+      const resultado = practica.obtenerPractica("Práctica ABC");
+      
+      expect(resultado).toBeUndefined(); 
+    });
+  
+  });
