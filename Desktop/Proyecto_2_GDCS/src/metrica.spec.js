f9c8d3ac2e87c9618440bf4b9c7a121e2ee7204c
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-10-16 16:08:32 -0700

    Pruebas para la funcion calcularPuntajePorCantidadLineas()

--- Desktop/Proyecto_2_GDCS/src/metrica.spec.js
+++ Desktop/Proyecto_2_GDCS/src/metrica.spec.js
@@ -65,3 +65,62 @@ describe("Test para cargarMetricas()", () => {
       expect(metrica.getPuntaje()).toEqual(0);
     });
   });
+
+  describe("Test para cargarMetricas()", () => {
+    it("Debe registrar las metricas cargadas con la funcion en la variable metrica si es convencional", () => {
+        let metrica = new Metrica(1, "Commit inicializado automaticamente");
+
+        metrica.cargarMetricas(100, 16, 80, "Bueno", 1);
+        expect(metrica.pruebas).toEqual(100);
+        expect(metrica.cantidadLineas).toEqual(16);
+        expect(metrica.cobertura).toEqual(80);
+        expect(metrica.complejidad).toEqual("Bueno");
+        expect(metrica.getFrecuencia()).toEqual(1);
+    });
+
+    it("Debe registrar las metricas con valores null, porque el tipo no es convencional", () => {
+      let metrica = new Metrica(1, "Commit inicializado automaticamente", "Refactorizacion");
+
+      metrica.cargarMetricas(100, 16, 80, "Bueno", 1);
+      expect(metrica.pruebas).toEqual(null);
+      expect(metrica.cantidadLineas).toEqual(null);
+      expect(metrica.cobertura).toEqual(null);
+      expect(metrica.complejidad).toEqual(null);
+      expect(metrica.getFrecuencia()).toEqual(null);
+      expect(metrica.getPuntaje()).toEqual(0);
+    });
+  });
+
+  describe("Test para calcularPuntajePorCantidadLineas()", () => {
+    it("Debe retornar el puntaje, calculandolo por la cantidad de lineas", () => {
+        let metrica = new Metrica(1, "Commit inicializado automaticamente");
+        let cantidadLineas = 10;
+
+        expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(20);
+    });
+
+    it("Debe retornar el puntaje, calculandolo por la cantidad de lineas", () => {
+      let metrica = new Metrica(1, "Commit inicializado automaticamente");
+      let cantidadLineas = 30;
+
+      expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(16);
+    });
+    it("Debe retornar el puntaje, calculandolo por la cantidad de lineas", () => {
+      let metrica = new Metrica(1, "Commit inicializado automaticamente");
+      let cantidadLineas = 50;
+
+      expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(12);
+    });
+    it("Debe retornar el puntaje, calculandolo por la cantidad de lineas", () => {
+      let metrica = new Metrica(1, "Commit inicializado automaticamente");
+      let cantidadLineas = 70;
+
+      expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(8);
+    });
+    it("Debe retornar el puntaje, calculandolo por la cantidad de lineas", () => {
+      let metrica = new Metrica(1, "Commit inicializado automaticamente");
+      let cantidadLineas = -1;
+
+      expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(0);
+    });
+  });
