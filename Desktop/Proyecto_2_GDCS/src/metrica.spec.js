c41804b6a47c54a9f2fd7d9737863d8a006a3eb2
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-10-18 08:33:19 -0700

    Pruebas para la funcion calcularPuntajePorComplejidad()

--- Desktop/Proyecto_2_GDCS/src/metrica.spec.js
+++ Desktop/Proyecto_2_GDCS/src/metrica.spec.js
@@ -42,119 +42,137 @@ describe("Test para getTipo()", () => {
   });
 
 describe("Tests para cargarMetricas()", () => {
-    it("Debe registrar las metricas cargadas con la funcion en la variable metrica si es convencional", () => {
-        let metrica = new Metrica(1, "Commit inicializado automaticamente");
+  it("Debe registrar las metricas cargadas con la funcion en la variable metrica si es convencional", () => {
+    let metrica = new Metrica(1, "Commit inicializado automaticamente");
 
-        metrica.cargarMetricas(100, 16, 80, "Bueno", 1);
-        expect(metrica.pruebas).toEqual(100);
-        expect(metrica.cantidadLineas).toEqual(16);
-        expect(metrica.cobertura).toEqual(80);
-        expect(metrica.complejidad).toEqual("Bueno");
-        expect(metrica.getFrecuencia()).toEqual(1);
-    });
-
-    it("Debe registrar las metricas con valores null, porque el tipo no es convencional", () => {
-      let metrica = new Metrica(1, "Commit inicializado automaticamente", "Refactorizacion");
-
-      metrica.cargarMetricas(100, 16, 80, "Bueno", 1);
-      expect(metrica.pruebas).toEqual(null);
-      expect(metrica.cantidadLineas).toEqual(null);
-      expect(metrica.cobertura).toEqual(null);
-      expect(metrica.complejidad).toEqual(null);
-      expect(metrica.getFrecuencia()).toEqual(null);
-      expect(metrica.getPuntaje()).toEqual(0);
-    });
+    metrica.cargarMetricas(100, 16, 80, "Bueno", 1);
+    expect(metrica.pruebas).toEqual(100);
+    expect(metrica.cantidadLineas).toEqual(16);
+    expect(metrica.cobertura).toEqual(80);
+    expect(metrica.complejidad).toEqual("Bueno");
+    expect(metrica.getFrecuencia()).toEqual(1);
   });
 
-  describe("Tests para cargarMetricas()", () => {
-    it("Debe registrar las metricas cargadas con la funcion en la variable metrica si es convencional", () => {
-        let metrica = new Metrica(1, "Commit inicializado automaticamente");
+  it("Debe registrar las metricas con valores null, porque el tipo no es convencional", () => {
+    let metrica = new Metrica(1, "Commit inicializado automaticamente", "Refactorizacion");
 
-        metrica.cargarMetricas(100, 16, 80, "Bueno", 1);
-        expect(metrica.pruebas).toEqual(100);
-        expect(metrica.cantidadLineas).toEqual(16);
-        expect(metrica.cobertura).toEqual(80);
-        expect(metrica.complejidad).toEqual("Bueno");
-        expect(metrica.getFrecuencia()).toEqual(1);
-    });
+    metrica.cargarMetricas(100, 16, 80, "Bueno", 1);
+    expect(metrica.pruebas).toEqual(null);
+    expect(metrica.cantidadLineas).toEqual(null);
+    expect(metrica.cobertura).toEqual(null);
+    expect(metrica.complejidad).toEqual(null);
+    expect(metrica.getFrecuencia()).toEqual(null);
+    expect(metrica.getPuntaje()).toEqual(0);
+  });
+});
 
-    it("Debe registrar las metricas con valores null, porque el tipo no es convencional", () => {
-      let metrica = new Metrica(1, "Commit inicializado automaticamente", "Refactorizacion");
+describe("Tests para calcularPuntajePorCantidadLineas()", () => {
+  it("Debe retornar el puntaje de 20, dado que las lineas aniadidas en el commit son 10", () => {
+      let metrica = new Metrica(1, "Commit inicializado automaticamente");
+      let cantidadLineas = 10;
 
-      metrica.cargarMetricas(100, 16, 80, "Bueno", 1);
-      expect(metrica.pruebas).toEqual(null);
-      expect(metrica.cantidadLineas).toEqual(null);
-      expect(metrica.cobertura).toEqual(null);
-      expect(metrica.complejidad).toEqual(null);
-      expect(metrica.getFrecuencia()).toEqual(null);
-      expect(metrica.getPuntaje()).toEqual(0);
-    });
+      expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(20);
   });
 
-  describe("Tests para calcularPuntajePorCantidadLineas()", () => {
-    it("Debe retornar el puntaje de 20, dado que las lineas aniadidas en el commit son 10", () => {
-        let metrica = new Metrica(1, "Commit inicializado automaticamente");
-        let cantidadLineas = 10;
+  it("Debe retornar el puntaje de 16, dado que las lineas aniadidas en el commit son 30", () => {
+    let metrica = new Metrica(1, "Commit inicializado automaticamente");
+    let cantidadLineas = 30;
 
-        expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(20);
-    });
+    expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(16);
+  });
+    
+  it("Debe retornar el puntaje de 12, dado que las lineas aniadidas en el commit son 50", () => {
+    let metrica = new Metrica(1, "Commit inicializado automaticamente");
+    let cantidadLineas = 50;
 
-    it("Debe retornar el puntaje de 16, dado que las lineas aniadidas en el commit son 30", () => {
+    expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(12);
+  });
+    
+  it("Debe retornar el puntaje de 8, dado que las lineas aniadidas en el commit son 70", () => {
+    let metrica = new Metrica(1, "Commit inicializado automaticamente");
+    let cantidadLineas = 70;
+
+    expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(8);
+  });
+    
+  it("Debe retornar el puntaje de 0, dado que las lineas aniadidas en el commit esten fuera del rango como numeros negativos", () => {
+    let metrica = new Metrica(1, "Commit inicializado automaticamente");
+    let cantidadLineas = -1;
+
+    expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(0);
+  });
+});
+
+describe("Tests para calcularPuntajePorCobertura()", () => {
+  it("Debe retornar el puntaje de 8, dado que el porcentaje de cobertura alcanzada es 50", () => {
       let metrica = new Metrica(1, "Commit inicializado automaticamente");
-      let cantidadLineas = 30;
+      let cobertura = 50;
 
-      expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(16);
-    });
-    it("Debe retornar el puntaje de 12, dado que las lineas aniadidas en el commit son 50", () => {
-      let metrica = new Metrica(1, "Commit inicializado automaticamente");
-      let cantidadLineas = 50;
-
-      expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(12);
-    });
-    it("Debe retornar el puntaje de 8, dado que las lineas aniadidas en el commit son 70", () => {
-      let metrica = new Metrica(1, "Commit inicializado automaticamente");
-      let cantidadLineas = 70;
-
-      expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(8);
-    });
-    it("Debe retornar el puntaje de 0, dado que las lineas aniadidas en el commit esten fuera del rango como numeros negativos", () => {
-      let metrica = new Metrica(1, "Commit inicializado automaticamente");
-      let cantidadLineas = -1;
-
-      expect(metrica.calcularPuntajePorCantidadLineas(cantidadLineas)).toEqual(0);
-    });
+      expect(metrica.calcularPuntajePorCobertura(cobertura)).toEqual(8);
   });
 
-  describe("Tests para calcularPuntajePorCobertura()", () => {
-    it("Debe retornar el puntaje de 8, dado que el porcentaje de cobertura alcanzada es 50", () => {
-        let metrica = new Metrica(1, "Commit inicializado automaticamente");
-        let cobertura = 50;
+  it("Debe retornar el puntaje de 12, dado que el porcentaje de cobertura alcanzada es 75", () => {
+    let metrica = new Metrica(1, "Commit inicializado automaticamente");
+    let cobertura = 75;
 
-        expect(metrica.calcularPuntajePorCobertura(cobertura)).toEqual(8);
-    });
-
-    it("Debe retornar el puntaje de 12, dado que el porcentaje de cobertura alcanzada es 75", () => {
-      let metrica = new Metrica(1, "Commit inicializado automaticamente");
-      let cobertura = 75;
-
-      expect(metrica.calcularPuntajePorCobertura(cobertura)).toEqual(12);
-    });
-    it("Debe retornar el puntaje de 16, dado que el porcentaje de cobertura alcanzada es 85", () => {
-      let metrica = new Metrica(1, "Commit inicializado automaticamente");
-      let cobertura = 85;
-
-      expect(metrica.calcularPuntajePorCobertura(cobertura)).toEqual(16);
-    });
-    it("Debe retornar el puntaje de 20, dado que el porcentaje de cobertura alcanzada es 100", () => {
-      let metrica = new Metrica(1, "Commit inicializado automaticamente");
-      let cobertura = 100;
-
-      expect(metrica.calcularPuntajePorCobertura(cobertura)).toEqual(20);
-    });
-    it("Debe retornar el puntaje de 0, dado que el porcentaje de cobertura alcanzada este fuera de los rangos como -1", () => {
-      let metrica = new Metrica(1, "Commit inicializado automaticamente");
-      let cobertura = -1;
-
-      expect(metrica.calcularPuntajePorCobertura(cobertura)).toEqual(0);
-    });
+    expect(metrica.calcularPuntajePorCobertura(cobertura)).toEqual(12);
   });
+
+  it("Debe retornar el puntaje de 16, dado que el porcentaje de cobertura alcanzada es 85", () => {
+    let metrica = new Metrica(1, "Commit inicializado automaticamente");
+    let cobertura = 85;
+
+    expect(metrica.calcularPuntajePorCobertura(cobertura)).toEqual(16);
+  });
+    
+  it("Debe retornar el puntaje de 20, dado que el porcentaje de cobertura alcanzada es 100", () => {
+    let metrica = new Metrica(1, "Commit inicializado automaticamente");
+    let cobertura = 100;
+
+    expect(metrica.calcularPuntajePorCobertura(cobertura)).toEqual(20);
+  });
+    
+  it("Debe retornar el puntaje de 0, dado que el porcentaje de cobertura alcanzada este fuera de los rangos como -1", () => {
+    let metrica = new Metrica(1, "Commit inicializado automaticamente");
+    let cobertura = -1;
+
+    expect(metrica.calcularPuntajePorCobertura(cobertura)).toEqual(0);
+  });
+});
+
+describe("Tests para calcularPuntajePorComplejidad()", () => {
+  it("Debe retornar el puntaje de 20, dado que la complejidad de funciones agregadas sea excelente", () => {
+      let metrica = new Metrica(1, "Commit inicializado automaticamente");
+      let complejidad = "Excelente";
+
+      expect(metrica.calcularPuntajePorComplejidad(complejidad)).toEqual(20);
+  });
+
+  it("Debe retornar el puntaje de 16, dado que la complejidad de funciones agregadas sea buena", () => {
+    let metrica = new Metrica(1, "Commit inicializado automaticamente");
+    let complejidad = "Bueno";
+
+    expect(metrica.calcularPuntajePorComplejidad(complejidad)).toEqual(16);
+  });
+
+  it("Debe retornar el puntaje de 12, dado que la complejidad de funciones agregadas sea regular", () => {
+    let metrica = new Metrica(1, "Commit inicializado automaticamente");
+    let complejidad = "Regular";
+
+    expect(metrica.calcularPuntajePorComplejidad(complejidad)).toEqual(12);
+  });
+    
+  it("Debe retornar el puntaje de 8, dado que la complejidad de funciones agregadas sea deficiente", () => {
+    let metrica = new Metrica(1, "Commit inicializado automaticamente");
+    let complejidad = "Deficiente";
+
+    expect(metrica.calcularPuntajePorComplejidad(complejidad)).toEqual(8);
+  });
+    
+  it("Debe retornar el puntaje de 0, dado que la complejidad de funciones no este registrada dentro de los paramentros como ninguna", () => {
+    let metrica = new Metrica(1, "Commit inicializado automaticamente");
+    let complejidad = "Ninguno";
+
+    expect(metrica.calcularPuntajePorComplejidad(complejidad)).toEqual(0);
+  });
+});
